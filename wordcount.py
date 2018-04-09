e95b201dda423a2f817fe21ac0da9cf6355862b0
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-09 12:59:07 -0700

    Done with Counter

--- wordcount.py
+++ wordcount.py
@@ -19,14 +19,36 @@
 
 
 
+# def word_count(filename):
+#     counts_words = {}
+#     with open(filename) as f:     
+#         for line in f:
+#             words = line.split()
+#             # print words
+#             for word in words:
+#                 counts_words[word] = counts_words.get(word, 0) + 1
+
+#     return counts_words
+
+# def iterating_dict(filename):
+#     counts_words = word_count(filename)
+#     for word, count in counts_words.iteritems():
+#         print word, count
+    
+# iterating_dict("twain.txt")
+
+
+from collections import Counter
+
 def word_count(filename):
-    counts_words = {}
+    # counts_words = {}
+    all_words = []
     with open(filename) as f:     
         for line in f:
             words = line.split()
-            # print words
-            for word in words:
-                counts_words[word] = counts_words.get(word, 0) + 1
+            all_words.extend(words)
+        counts_words = Counter(all_words)
+          
 
     return counts_words
 
@@ -36,3 +58,4 @@ def iterating_dict(filename):
         print word, count
     
 iterating_dict("twain.txt")
+
