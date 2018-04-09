3188b172bec656ab0f10e73598d62b1d26ade07f
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-09 12:27:55 -0700

    Finished funtions in file. All are working properly.

--- wordcount.py
+++ wordcount.py
@@ -1,13 +1,38 @@
-# put your code here.
+
+# def word_count(filename):
+#     with open(filename) as f:
+#         counts_words = {}
+#         for line in f:
+#             words = line.split()
+#             # print words
+#             for word in words:
+#                 counts_words[word] = counts_words.get(word, 0) + 1
+
+#         return counts_words
+
+# print word_count("test.txt")
+
+
+
+
+
+
+
+
 def word_count(filename):
-    with open(filename) as f:
-        counts_words = {}
+    counts_words = {}
+    with open(filename) as f:     
         for line in f:
             words = line.split()
             # print words
             for word in words:
                 counts_words[word] = counts_words.get(word, 0) + 1
 
-        return counts_words
+    return counts_words
 
-print word_count("test.txt")
+def iterating_dict(filename):
+    counts_words = word_count(filename)
+    for word, count in counts_words.iteritems():
+        print word, count
+    
+iterating_dict("twain.txt")
